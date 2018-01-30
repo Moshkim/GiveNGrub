c0c99a53082f974191ab68f9d308b506e2e382f5
Moses <rlakim@ucdavis.edu>
2018-01-30 12:13:15 -0800

    MVC folder structure and define the role of each file

---
+++ server.js
@@ -0,0 +1,25 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+
+
+let app = express()
+const PORT = process.env.PORT || 5000
+
+let database = require('./models')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+
+//If there is more routes then you can add more~
+
+require('./controls/routes/routes.js')(app)
+require('./controls/routes/routes.js')(app)
+require('./controls/routes/routes.js')(app)
+
+database.sequelize.sync({force: false}).then(function(){
+    app.listen(PORT, function() {
+        console.log(`App is listening on PORT: ${PORT}`)
+    })
+})
