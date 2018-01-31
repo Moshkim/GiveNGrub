5d07dfa240ec22bb3316e054017fd5fa7fd0c6c4
Richard Chov <richardchov@dhcp-v065-118.mobile.uci.edu>
2018-01-30 21:15:26 -0800

    html-routes and sign-in page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let bodyParser = require('body-parser')
 let app = express()
 const PORT = process.env.PORT || 5000
 
-let database = require('./models')
+//let database = require('./models')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -14,12 +14,12 @@ app.use(express.static('public'))
 
 //If there is more routes then you can add more~
 
-require('./controls/routes/routes.js')(app)
-require('./controls/routes/routes.js')(app)
-require('./controls/routes/routes.js')(app)
+require('./controllers/html-routes.js')(app)
+// require('./controls/routes/routes.js')(app)
+// require('./controls/routes/routes.js')(app)
 
-database.sequelize.sync({force: false}).then(function(){
+//database.sequelize.sync({force: false}).then(function(){
     app.listen(PORT, function() {
         console.log(`App is listening on PORT: ${PORT}`)
     })
-})
+//})
