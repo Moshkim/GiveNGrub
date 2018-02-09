73109e1ee4736ec33a07f0abdb44790cddb19cf5
Moses <rlakim@ucdavis.edu>
2018-02-08 16:53:43 -0800

    This is epic that we can click one of the lists and put it into grabbed list

--- controllers/routes/nonprofit-api-routes.js
+++ controllers/routes/nonprofit-api-routes.js
@@ -2,6 +2,51 @@
 let database = require("../../models")
 
 module.exports = function(app){
+
+
+    app.get('/api/np/info/:id', function(req, res){
+        database.Organization.findOne({
+            where:{
+                id: req.params.id
+            } 
+        }).then(function(result){
+            console.log(result)
+            res.json(result)
+        })
+    })
+
+    app.get('/api/np/update/foodlist/:id', function(req, res){
+        database.Food.update(
+            {
+                takenBy: req.body.myId
+            },
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        ).then(function(result){
+            res.json(result)
+        })
+    })
+
+    app.put('/api/update/stars/:id', function(req, res){
+        database.Organization.update(
+            {
+                rating: req.body.rate
+            },
+            {
+                where:{
+                    id: req.params.id
+                }
+            }
+        ).then(function(result){
+            //console.log('\n\n' + result.rating)
+            res.json(result)
+
+        })
+    })
+
     app.get('/api/np/foodlist', function(req, res){
 
         database.Food.findAll({
@@ -15,7 +60,7 @@ module.exports = function(app){
                 attributes: ['company_name', 'contact', 'address', 'latitude', 'longitude', 'rating']
             }]
         }).then(function(foodList){
-            console.log(foodList)
+            //console.log(foodList)
             res.json(foodList)
         })
     })
--- views/js/nonprofit.js
+++ views/js/nonprofit.js
@@ -1,13 +1,87 @@
 $(document).ready(function(){
 
 
-    function getFoodListNearYou() {
 
+
+    $(document).on('click', '.checkboxes', function(event){
+        console.log('\n\n')
+
+        console.log(this)
+        
+        console.log($(this).attr("org_name"))
+
+        console.log(this.address)
+        console.log('\n\n')
+
+        let id = this.id
+        
+        /*
+        let rate = {
+            rate: rating + 1
+        }
+        $.ajax({
+            method: "PUT",
+            url: "/api/np/update/foodlist/" + id,
+            data: rate
+        }).then(function(result){
+            console.log(result)
+        })
+        */
+
+        if($("#"+this.id).is(":checked")){
+            console.log("hi!")
+
+
+            $('#grabLists').append($('<div>')
+                .addClass("card blue-grey darken-1")
+                .append($('<div>')
+                    .addClass("card-content white-text")
+                    .append($('<span>')
+                        .addClass("card-title")
+                            .text($(this).attr("org_name")))
+                    .append($('<p>')
+                        .text(`Address: ${$(this).attr("address")}`))
+                    .append($('<p>')
+                        .text(`Description: ${$(this).attr("description")}`)))
+                .append($('<div>')
+                    .addClass("card-action")
+                    .append($('<button>')
+                        .addClass("btn waves-effect waves-light")
+                        .attr("type", "submit")
+                        .attr("id", this.id)
+                        .text("Submit"))))
+
+            $("."+this.id).empty()
+
+            
+        }
+        
+        
+    })
+
+    function grabOrganizationInfo() {
         let identityObject = localStorage.getItem('identity')
         identityObject = JSON.parse(identityObject)
 
-        console.log(identityObject.state)
-        console.log(identityObject.city)
+        $.get('/api/np/info/'+identityObject.id, function(data, status){
+            if(data){
+                $('#name').text(data.company_name)
+            }
+        })
+    }
+
+    function returnId() {
+        let identityObject = localStorage.getItem('identity')
+        identityObject = JSON.parse(identityObject)
+        return identityObject.id
+    }
+
+    function getFoodListNearYou() {
+        let identityObject = localStorage.getItem('identity')
+        identityObject = JSON.parse(identityObject)
+
+        //console.log(identityObject.state)
+        //console.log(identityObject.city)
 
         let filter = {
             state: identityObject.state,
@@ -17,7 +91,7 @@ $(document).ready(function(){
         $.get('/api/np/foodlist', filter, function(data, status){
 
             if(data){
-                console.log(data)
+                //console.log(data)
 
                 for(let i = 0; i < data.length; i++){
                     $('#data').append($('<li>')
@@ -29,60 +103,32 @@ $(document).ready(function(){
                             .append($('<p>')
                                 .text(`Donor Ratings: ${data[i].Organization.rating}`))
                             .append($('<p>')
-                                .text(`Food Donated : \n Frozen: ${data[i].frozen}, Fresh: ${data[i].fresh}, Canned: ${data[i].canned}, Packaged: ${data[i].packaged}`))))
+                                .text(`<Food Donated>`))
+                            .append($('<p>')
+                                .text(`Frozen: ${data[i].frozen}, Fresh: ${data[i].fresh}, Canned: ${data[i].canned}, Packaged: ${data[i].packaged}`))
+                            .append($('<p>')
+                                .text(`Description: ${data[i].description}`))
+                            .append($('<p>')
+                                .text(`Address: ${data[i].Organization.address}`))
+                            .append($('<p>')
+                                .text(`Contacts: ${data[i].Organization.contact}`))
+                            .append($('<div>')
+                                .append($('<input>').attr('type', "checkbox").addClass('checkboxes')
+                                    .attr("id", data[i].id)
+                                    .attr("organizationId", data[i].OrganizationId)
+                                    .attr("org_name", data[i].Organization.company_name)
+                                    .attr("address", data[i].Organization.address)
+                                    .attr("description", data[i].description))
+                                .append($('<label>').attr('for', data[i].id).text("Grab")))).addClass(""+data[i].id))
+                                
+                            
                 }
             }
         })
 
     }
 
-
-
-
-
+    grabOrganizationInfo()
     getFoodListNearYou()
 
-
-
-
-    function getFoodProviderFoodList(){
-        let identityObject = localStorage.getItem('identity')
-        identityObject = JSON.parse(identityObject)
-        getTheData(identityObject.id)
-    }
-
-
-    function getTheData(id) {
-    
-        $.get('/api/fp/food/'+ id, function(data, status){
-        
-            if(data){
-                //console.log(data)
-                for(let i = 0; i < data.length; i++){
-                    let status
-
-                    if(data[i].status === false){
-                        status = "Give"
-                    } else {
-                        status = "Grabbed"
-                    }
-
-                    $("#submitNewList").append($("<tr>")
-                        .append($("<td>").text(status))
-                        .append($("<td>").text(data[i].createdAt))
-                        .append($("<td>").text(data[i].fresh))
-                        .append($("<td>").text(data[i].frozen))
-                        .append($("<td>").text(data[i].packaged))
-                        .append($("<td>").text(data[i].canned))
-                        .append($("<td>").text(data[i].description)))
-                    
-                }
-            } else {
-                console.log("You do not have anything listed!")
-            }
-        })
-
-    }
-
-    //getFoodProviderFoodList()
 })
